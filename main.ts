1a89af56b6241b0e28505f6c364003220c16a2e8
camilinpiniguin15 <137525905+camilinpiniguin15@users.noreply.github.com>
2023-06-23 10:54:28 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 basic.forever(function () {
+    music.play(music.stringPlayable("B - - - B - - - ", 120), music.PlaybackMode.LoopingInBackground)
     basic.showIcon(IconNames.Heart)
     basic.showIcon(IconNames.SmallHeart)
 })
